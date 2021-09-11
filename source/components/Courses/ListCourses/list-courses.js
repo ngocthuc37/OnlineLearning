f01e9dcbacce8d68d104f1b9327f77347f8245ee
ngocthuc37 <trieumaingocthuc37@gmail.com>
2021-09-11 16:10:45 +0700

    list courses by sectionlist

--- source/components/Courses/ListCourses/list-courses.js
+++ source/components/Courses/ListCourses/list-courses.js
@@ -1,34 +1,97 @@
 import React from 'react';
-import {View, FlatList, TextInput, Button} from 'react-native';
+import {View, FlatList, TextInput, Button, SectionList, Text} from 'react-native';
 import ListCoursesItems from '../ListCoursesItems/list-courses-items'
 
 
 const ListCourses = (props) => { 
     const courses = [
     {
-        id: 1, 
-        title: 'React Native',
-        author: 'Thuc Trieu',
-        level: 'Advance',
-        released: 'December 9, 2021', 
-        duration: '40 hours'
+        title: 'Mobile', 
+        data: [
+            {
+                id: 1, 
+                title: 'React Native',
+                author: 'Thuc Trieu',
+                level: 'Advance',
+                released: 'December 9, 2021', 
+                duration: '40 hours'
+            },
+            {
+                id: 2, 
+                title: 'iOS',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021', 
+                duration: '30 hours'
+            },
+            {
+                id: 3, 
+                title: 'android',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021',   
+                duration: '30 hours'
+            }
+        ]
     },
     {
-        id: 2, 
-        title: 'iOS',
-        author: 'Thuc Trieu Dep Trai',
-        level: 'Beginner',
-        released: 'December 8, 2021', 
-        duration: '30 hours'
+        title: 'Web',
+        data: [
+            {
+                id: 1, 
+                title: 'ASP.net',
+                author: 'Thuc Trieu',
+                level: 'Advance',
+                released: 'December 9, 2021', 
+                duration: '40 hours'
+            },
+            {
+                id: 2, 
+                title: 'React',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021', 
+                duration: '30 hours'
+            },
+            {
+                id: 3, 
+                title: 'PHP',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021',   
+                duration: '30 hours'
+            }
+        ]
     },
     {
-        id: 3, 
-        title: 'android',
-        author: 'Thuc Trieu Dep Trai',
-        level: 'Beginner',
-        released: 'December 8, 2021',   
-        duration: '30 hours'
-    }
+        title: 'Scrath',
+        data: [
+            {
+                id: 1, 
+                title: 'SB',
+                author: 'Thuc Trieu',
+                level: 'Advance',
+                released: 'December 9, 2021', 
+                duration: '40 hours'
+            },
+            {
+                id: 2, 
+                title: 'SA',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021', 
+                duration: '30 hours'
+            },
+            {
+                id: 3, 
+                title: 'SD',
+                author: 'Thuc Trieu Dep Trai',
+                level: 'Beginner',
+                released: 'December 8, 2021',   
+                duration: '30 hours'
+            }
+        ]
+    },
     ]
 
     const searchView = () => { 
@@ -40,11 +103,16 @@ const ListCourses = (props) => {
         </View>
     }
     return <View>
-        <FlatList
-            data = {courses}
+       
+        <SectionList
+            sections = {courses}
             renderItem = {({item}) => <ListCoursesItems item={item} />}
+            renderSectionHeader = {({section: {title}}) => (<View style ={{backgroundColor: 'white'}}>
+                                                                <Text style ={{backgroundColor: 'white', fontSize: 24}}>{title}</Text> 
+                                                            </View>)
+                                    }
             ListHeaderComponent={()=> searchView()}
-            />
+        />
     </View>
 
 }
